fa6119cade37bbb783c756291b6f3c8f77c70c7c
Shane Hughes <shanehughes314@gmail.com>
2017-03-04 09:21:49 -0600

    api routes

--- server/routes.js
+++ server/routes.js
@@ -32,23 +32,81 @@ router.get("/auth/logout", (req, res) => {
  */
 
 router.get("/api/all", (req, res) => {
-	
+	db.getAllMemes({
+		offset: req.query.offset,
+		limit: req.query.limit
+	}, (err, memes) => {
+		if (err) {
+			console.error(err);
+			res.sendStatus(500);
+		} else {
+			res.json({ memes: memes });
+		}
+	});
 });
 
 router.get("/api/:userId", (req, res) => {
-	// get user memes
+	db.getUserMemes(req.user.id, (err, memes) => {
+		if (err) {
+			console.error(err);
+			res.sendStatus(500);
+		} else {
+			res.json({ memes: memes });
+		}
+	})
 });
 
 router.post("/api/:userId", (req, res) => {
-	// new meme
+	db.newMeme(req.user.id, req.body.meme, (err, meme) => {
+		if (err) {
+			console.error(err);
+			res.sendStatus(500);
+		} else {
+			res.json({ meme: meme });
+		}
+	})
 });
 
 router.put("/api/:userId/:memeId", (req, res) => {
-	// edit meme
+	if (req.user.id != req.params.userId) {
+		res.sendStatus(401);
+	} else {
+		db.editMeme(req.user.id, req.params.memeId,
+		req.body.edits, (err, meme) => {
+			if (err) {
+				if (err.message == "Unauthorized") {
+					res.sendStatus(401);
+				} else if (err.message == "No meme found") {
+					res.sendStatus(400);
+				} else {
+					console.error(err);
+					res.setStatus(500);
+				}
+			} else {
+				res.json({ meme: meme });
+			}
+		})
+	}
 });
 
 router.delete("/api/:userId/:memeId", (req, res) => {
-	// delete meme
+	if (req.user.id != req.params.userId) {
+		res.sendStatus(401);
+	} else {
+		db.deleteMeme(req.user.id, req.params.memeId,
+		(err, meme) => {
+			if (err) {
+				if (err.message == "Unauthorized") {
+					res.sendStatus(401);
+				} else {
+					console.error(err);
+					res.sendStatus(500);
+				}
+			} else {
+				res.json({ meme: meme });
+			}
+		})
+	}
 });
 
 /*
